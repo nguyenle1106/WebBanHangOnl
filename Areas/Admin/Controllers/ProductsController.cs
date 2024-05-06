7e1f988c08e44ccdbcd2125a59de52fca42735a4
nguyenle1106 <153020288+nguyenle1106@users.noreply.github.com>
2024-05-06 22:10:21 +0700

    Initial commit

---
+++ Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,132 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+using WebBanHangOnline.Models.EF;
+
+namespace WebBanHangOnline.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin, Employee")]
+    public class ProductsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin/Products
+        public ActionResult Index(int? page)
+        {
+            IEnumerable<Product> items = db.Products.OrderByDescending(x => x.ID);
+            var pageSize = 10;
+            if (page == null)
+            {
+                page = 1;
+            }
+            var pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            items = items.ToPagedList(pageIndex, pageSize);
+            ViewBag.PageSize = pageSize;
+            ViewBag.Page = page;
+            return View(items);
+        }
+
+        public ActionResult Add() 
+        {
+            ViewBag.ProductCategory = new SelectList(db.ProductCategories.ToList(),"ID","Title");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(Product model, List<string> Images, List<int> rDefault)
+        {
+            if(ModelState.IsValid)
+            {
+                if(Images != null && Images.Count > 0)
+                {
+                    for(int i = 0; i<Images.Count; i++)
+                    {
+                        if (i + 1 == rDefault[0])
+                        {
+                            model.Image = Images[i];    
+                            model.ProductImage.Add(new ProductImage
+                            {
+                                ProductID = model.ID,
+                                Image = Images[i],
+                                IsDefault = true
+                            });
+                        }
+                        else
+                        {
+                            model.ProductImage.Add(new ProductImage
+                            {
+                                ProductID = model.ID,
+                                Image = Images[i],
+                                IsDefault = false
+                            });
+                        }
+                    }
+                }
+                model.CreatedDate = DateTime.Now;
+                model.ModifiedDate = DateTime.Now;
+                if (string.IsNullOrEmpty(model.SeoTitle))
+                {
+                    model.SeoTitle = model.Title;
+                }
+                    if (string.IsNullOrEmpty(model.Alias))
+                model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
+                db.Products.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProductCategory = new SelectList(db.ProductCategories.ToList(), "ID", "Title");
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            ViewBag.ProductCategory = new SelectList(db.ProductCategories.ToList(), "ID", "Title");
+            var item = db.Products.Find(id);
+            return View(item);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.ModifiedDate = DateTime.Now;
+                model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
+                db.Products.Attach(model);
+                db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var item = db.Products.Find(id);
+            if (item != null)
+            {
+                var checkImg = item.ProductImage.Where(x => x.ProductID == item.ID);
+                if(checkImg != null)
+                {
+                    foreach(var img in checkImg)
+                    {
+                        db.ProductImages.Remove(img);
+                        db.SaveChanges();
+                    }
+                }
+                db.Products.Remove(item);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+
+            }
+}
